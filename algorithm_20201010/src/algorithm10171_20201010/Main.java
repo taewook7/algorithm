6459382907878f910abaecbb20ba281710b2c398
taewook kim <x_o_d_n@naver.com>
2020-10-10 09:46:46 +0900

    Add files via upload

---
+++ algorithm_20201010/src/algorithm10171_20201010/Main.java
@@ -0,0 +1,11 @@
+package algorithm10171_20201010;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
---
+++ algorithm_20201010/src/algorithm10718_20201010/Main.java
@@ -0,0 +1,9 @@
+package algorithm10718_20201010;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}
