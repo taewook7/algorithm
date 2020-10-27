d36b133d8b7c8818fa2d2ec29f49fc3b8c8134ac
taewook kim <x_o_d_n@naver.com>
2020-10-27 09:13:44 +0900

    15596번 정수N개의 합
    TEST

---
+++ Algorithm_20201027/src/Test.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	public static long sum(int a[]) 
+	{
+		long result=0;	
+		for (int i = 0; i < a.length; i++) {
+			result=result+a[i];
+		}
+		
+		return result;
+	}
+}
