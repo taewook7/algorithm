ae0674fae04e6ac176ac750e63f3a9f6f10aa151
taewook kim <x_o_d_n@naver.com>
2020-12-05 09:08:40 +0900

    와이버스부릉부릉

---
+++ 20201205_baekjoon_14645_java/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N=sc.nextInt();
+		int K=sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			int A=sc.nextInt();
+			int B=sc.nextInt();
+		}
+		System.out.println("ºñ¿ÍÀÌ");
+	}
+
+}
