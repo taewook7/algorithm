83a6dc3ab269d8a2865840a7d30878ac156c2e6c
taewook kim <x_o_d_n@naver.com>
2020-12-15 09:56:46 +0900

    운동장 한 바퀴

---
+++ 20201215_baekjoon_16486_java/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		double pi=3.141592d;
+		
+
+		
+		System.out.println(2*a + 2*b*pi);
+		
+	}
+}
