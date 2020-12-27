cf2e0deca3894b7adffad48aacaa33dad3a6bef5
taewook kim <x_o_d_n@naver.com>
2020-12-27 10:26:29 +0900

    두수비교하기

---
+++ 20201227_baekjoon_1330_javascript/test.js
@@ -0,0 +1,12 @@
+var set=require("fs");
+var input = set.readFileSync("/dev/stdin").toString().split(" ");
+const a = Number(input[0]);
+const b = Number(input[1]);
+
+if(a<b) {
+    console.log("<");
+} else if(a>b) {
+    console.log(">");
+} else {
+    console.log("==");
+}
