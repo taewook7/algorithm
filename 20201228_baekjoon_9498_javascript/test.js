8bec7205efb8932824413c4f929fc51db3bd4d98
taewook kim <x_o_d_n@naver.com>
2020-12-28 23:55:38 +0900

    시험 성적

---
+++ 20201228_baekjoon_9498_javascript/test.js
@@ -0,0 +1,15 @@
+var set=require("fs");
+var input = set.readFileSync("/dev/stdin").toString().split(" ");
+const a = Number(input[0]);
+
+if(100>=a && a>=90) {
+    console.log("A");
+} else if(89>=a && a>=80) {
+    console.log("B");
+} else if(79>=a && a>=70) {
+    console.log("C");
+} else if(69>=a && a>=60) {
+    console.log("D");
+} else {
+    console.log("F");
+}
