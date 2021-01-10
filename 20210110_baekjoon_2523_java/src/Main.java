fb8c1ffd85d0064cb79944de468f6d512aff2e5d
taewook kim <x_o_d_n@naver.com>
2021-01-10 11:14:24 +0900

    별 찍기 -13

---
+++ 20210110_baekjoon_2523_java/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args)  {
+		Scanner sc = new Scanner(System.in); 
+		int n = sc.nextInt(); 
+		for (int i=1; i<=2*n-1; i++) { 
+			if (i<=n) { 
+				for(int j=0; j<i; j++) 
+					System.out.print("*"); 
+				System.out.println(); 
+			}else { 
+				for(int j=0; j<2*n-i; j++) 
+					System.out.print("*"); 
+				System.out.println(); 
+			} 
+		}
+	}
+}
