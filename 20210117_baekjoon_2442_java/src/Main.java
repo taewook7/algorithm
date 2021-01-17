894145494742e86c3504e80bdbd06a7c3cec0502
taewook kim <x_o_d_n@naver.com>
2021-01-17 10:24:33 +0900

    별 찍기-5

---
+++ 20210117_baekjoon_2442_java/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j<= n-i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k < (i*2)-1; k++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+	}
+}
