37776b7a339781fd9620d01ef40d86d89e4c9b33
taewook kim <x_o_d_n@naver.com>
2021-01-25 23:17:00 +0900

    단어 길이 재기

---
+++ 20210125_baekjoon_2743_java/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a=sc.next();
+		int count =0;
+		
+		for (int i = 0; i < a.length(); i++) {
+			a.charAt(i);
+			count++;
+		}
+		System.out.println(count);
+		
+	}
+}
