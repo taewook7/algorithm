f761bb176ba1275b71627404318fe40cf98aa700
taewook kim <x_o_d_n@naver.com>
2021-02-05 23:00:35 +0900

    그대로 출력하기2

---
+++ 20210205_baekjoon_11719_java/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args)  {
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNextLine()) {
+			String st = sc.nextLine();
+			System.out.println(st);
+		}
+	}
+
+}
